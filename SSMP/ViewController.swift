633853b95bbf6be0332ea5a989cdff36fd51f537
Ethan Lipnik <ethanlipnik@gmail.com>
2018-11-23 18:51:30 -0500

    Initial commit

---
+++ SSMP/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  SSMP
+//
+//  Created by Ethan Lipnik on 11/23/18.
+//  Copyright © 2018 Fetch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
