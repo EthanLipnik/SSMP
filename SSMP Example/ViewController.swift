7bf7cebb9e44fc76792855ff277b98ec344e9c2f
Ethan Lipnik <ethanlipnik@gmail.com>
2018-11-23 19:40:03 -0500

    Initial Commit

---
+++ SSMP Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SSMP Example
+//
+//  Created by Ethan Lipnik on 11/23/18.
+//  Copyright © 2018 Fetch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
